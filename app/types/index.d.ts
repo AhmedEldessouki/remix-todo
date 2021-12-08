cbaa646eb7f2db1e8c626501a0212e01cf771ab5
Ahmed ElDessouki <nemoahmed@hotmail.co.uk>
2021-12-08 19:09:39 +0300

    type name change needs a find and replace

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,14 +1,20 @@
 type Lists = Array<{name: string; url: string; id: string}>
 
-type ListData = {name: string; isDone: boolean; id: string; description: string}
+type TaskData = {name: string; isDone: boolean; id: string; description: string}
 
-type ListReminders = {id: string; todoId: string; start: Date; end: Date}
+type TaskReminder = {id: string; todoId: string; start: Date; end: Date}
 
 type List = {
-  tasks: Array<ListData>
-  reminders: Array<ListReminders>
+  tasks: Array<TaskData>
+  reminders: Array<TaskReminder>
 }
 
 type ObjectOfStrings = {[key: string]: string}
 
-export {Lists, List, ListData, ListReminders, ObjectOfStrings}
+export {
+  Lists,
+  List,
+  TaskData as ListData,
+  TaskReminder as ListReminders,
+  ObjectOfStrings,
+}
