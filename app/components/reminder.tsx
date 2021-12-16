f4ecfdd8b18ff39213cb10d62e298eb8fb9535d0
Ahmed ElDessouki <nemoahmed@hotmail.co.uk>
2021-12-16 14:45:29 +0300

    styling reminder styling for the on-going mode (half way)

--- app/components/reminder.tsx
+++ app/components/reminder.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import {v4} from 'uuid'
 
-interface LeftTime {
+type TimerType = {
   days: number
   hours: number
   minutes: number
@@ -12,17 +13,14 @@ const handleCountDown = (
   reminder: number,
   today: number | undefined = Date.now(),
 ) => {
-  if (typeof window === 'undefined') return
-
-  const left: LeftTime = {
+  const left: TimerType = {
     days: 0,
     hours: 0,
     minutes: 0,
     seconds: 0,
     status: reminder - today < 0 ? 'passed' : 'on-going',
   }
-
-  //   if (left.status === 'passed') return left
+  if (left.status === 'passed') return left
 
   left.days = new Date(today - reminder).getDay()
   left.hours = new Date(reminder - today).getHours()
@@ -43,9 +41,7 @@ function ReminderDisplay({
   start: number
   end: number
 }) {
-  const [timer, setTimer] = React.useState(() => {
-    return handleCountDown(end)
-  })
+  const [timer, setTimer] = React.useState(() => handleCountDown(end))
 
   React.useEffect(() => {
     setInterval(() => {
@@ -57,7 +53,21 @@ function ReminderDisplay({
 
   return (
     <div className="reminder__container" key={id + taskId} itemID={id + taskId}>
-      {JSON.stringify(timer, null, 2)}
+      <div className="reminder-text__container">
+        <span>{timer.status}</span>
+        <span>{new Date(end).toLocaleDateString()}</span>
+      </div>
+      <div className="count-down__container">
+        {Object.entries(timer).map(([key, value], i) => {
+          if (typeof value === 'string') return null
+          return (
+            <div className="count-down__sub-container" key={`${id}-${i}`}>
+              <p>{key}</p>
+              <p className="numbers">{`${value}`.padStart(2, '0')}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
